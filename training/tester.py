b8c29028b89aaef4d552b7331d17671c1c80c458
Yunshuang Yuan <yuanyunshuang@163.com>
2025-10-22 14:27:25 +0200

    update

--- training/tester.py
+++ training/tester.py
@@ -53,77 +53,10 @@ from training.utils.train_utils import (
 )
 
 
-CORE_LOSS_KEY = "core_loss"
-
-
-def unwrap_ddp_if_wrapped(model):
-    if isinstance(model, torch.nn.parallel.DistributedDataParallel):
-        return model.module
-    return model
-
-
-@dataclass
-class OptimAMPConf:
-    enabled: bool = False
-    amp_dtype: str = "float16"
-
-
-@dataclass
-class OptimConf:
-    optimizer: torch.optim.Optimizer = None
-    options: Optional[Dict[str, Any]] = None
-    param_group_modifiers: Optional[List] = None
-    amp: Optional[Dict[str, Any]] = None
-    gradient_clip: Any = None
-    gradient_logger: Any = None
-
-    def __post_init__(self):
-        # amp
-        if not isinstance(self.amp, OptimAMPConf):
-            if self.amp is None:
-                self.amp = {}
-            assert isinstance(self.amp, Mapping)
-            self.amp = OptimAMPConf(**self.amp)
-
-
-@dataclass
-class CudaConf:
-    cudnn_deterministic: bool = False
-    cudnn_benchmark: bool = True
-    allow_tf32: bool = False
-    # if not None, `matmul_allow_tf32` key will override `allow_tf32` for matmul
-    matmul_allow_tf32: Optional[bool] = None
-    # if not None, `cudnn_allow_tf32` key will override `allow_tf32` for cudnn
-    cudnn_allow_tf32: Optional[bool] = None
-
-
-@dataclass
-class CheckpointConf:
-    save_dir: str
-    save_freq: int
-    save_list: List[int] = field(default_factory=list)
-    model_weight_initializer: Any = None
-    save_best_meters: List[str] = None
-    skip_saving_parameters: List[str] = field(default_factory=list)
-    initialize_after_preemption: Optional[bool] = None
-    # if not None, training will be resumed from this checkpoint
-    resume_from: Optional[str] = None
-
-    def infer_missing(self):
-        if self.initialize_after_preemption is None:
-            with_skip_saving = len(self.skip_saving_parameters) > 0
-            self.initialize_after_preemption = with_skip_saving
-        return self
-    
-
-@dataclass
-class LoggingConf:
-    log_dir: str
-    log_level_primary: str = "INFO"
-    log_level_secondary: str = "ERROR"
-    log_scalar_frequency: int = 100
-    log_visual_frequency: int = 100
-    scalar_keys_to_log: Optional[Dict[str, Any]] = None
+from training.trainer import (
+    CORE_LOSS_KEY, unwrap_ddp_if_wrapped,
+    LoggingConf, CheckpointConf, OptimConf
+)
 
 
 class Tester:
